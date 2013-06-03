2f4d10e20ce48f7eb4cb9f570313bc90b683f104
Hannes Fostie <hannes@openminds.be>
2013-06-03 14:22:50 +0200

    Add start_at order to future scope

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -5,7 +5,7 @@ class Gathering < ActiveRecord::Base
   has_many :members, through: :participations
   belongs_to :owner, class_name: 'Member'
 
-  scope :future, where("start_at > '#{Time.now.beginning_of_day}'")
+  scope :future, where("start_at > '#{Time.now.beginning_of_day}'").order('start_at ASC')
 
   validates_presence_of :name, :location, :start_at, :description, :kind
 end
