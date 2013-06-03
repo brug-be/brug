eb489fd6c41b3333484106dfe756eefa9c7e8596
Hannes Fostie <hannes@openminds.be>
2013-06-03 15:42:52 +0200

    Properly handle 'no upcoming events' case

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def landing
     @gatherings = Gathering.future
+    @next_gathering = @gatherings.try(:first)
     render :layout => 'landing'
   end
 
