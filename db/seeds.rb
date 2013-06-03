58b7fb397faf0057d7f5f9a554770829e3d7b9b4
Hannes Fostie <hannes@openminds.be>
2013-06-03 16:25:35 +0200

    Remove all db data when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Gathering.delete_all
+Member.delete_all
+Participation.delete_all
+
 %w[hannesfostie joren jeroen_j rkh tenderlove dhh ev zhann holman qrush coreyhaines kytrinyx junkiesxl phoet rodreegez atog philnash ryanstenhouse mediafinger floordrees teabass mislav avdi].each { |username| Member.create_from_username username }
 
 lipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
