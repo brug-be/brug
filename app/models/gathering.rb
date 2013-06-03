c0624d9ec3f2f53ca5fefeecfed33b443a4354c2
Hannes Fostie <hannes@openminds.be>
2013-06-03 17:06:29 +0200

    Add gathering#participation method

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -8,4 +8,15 @@ class Gathering < ActiveRecord::Base
   scope :future, where("start_at > '#{Time.now.beginning_of_day}'").order('start_at ASC')
 
   validates_presence_of :name, :location, :start_at, :description, :kind
+
+  def participation
+    case members.count
+    when 0
+      "No one is attending yet"
+    when 1
+      "1 person attending"
+    else
+      "#{members.count} people attending"
+    end
+  end
 end
