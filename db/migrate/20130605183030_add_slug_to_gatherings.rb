472c661c628eccb6cfdc8984a9268ab756bc4cfc
Hannes Fostie <hannes@openminds.be>
2013-06-08 21:43:43 +0200

    Remove saving of all Gathering records in a migration.
    This would break migrations for all new developers on the project.

--- db/migrate/20130605183030_add_slug_to_gatherings.rb
+++ db/migrate/20130605183030_add_slug_to_gatherings.rb
@@ -2,6 +2,5 @@ class AddSlugToGatherings < ActiveRecord::Migration
   def change
     add_column :gatherings, :slug, :string
     add_index  :gatherings, :slug, unique: true
-    Gathering.all.map(&:save)
   end
 end
