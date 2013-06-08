d50bcb8d320b87f8b2d8412a759b53caf2ad434a
Joren De Groof <git@jorendegroof.be>
2013-06-08 22:04:02 +0200

    Removes test view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def test
-  end
-
   private
   def current_member
     @current_member ||= Member.find(session[:member_id]) if session[:member_id]
