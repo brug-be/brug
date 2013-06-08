8b89cb454383119063f7e75380fcbd18a793a693
Joren De Groof <git@jorendegroof.be>
2013-06-08 22:11:25 +0200

    Lists all past and upcoming events the member is joining

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -6,6 +6,7 @@ class Gathering < ActiveRecord::Base
   belongs_to :owner, class_name: 'Member'
 
   scope :future, -> { where("start_at > '#{Time.now.beginning_of_day}'").order('start_at ASC') }
+  scope :past, -> { where("start_at < '#{Time.now.beginning_of_day}'").order('start_at ASC') }
 
   validates_presence_of :name, :location, :start_at, :description, :kind
 
