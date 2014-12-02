f4dd766ba6bffb7d34b9e58b402b9e73484496c0
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2014-12-02 22:49:31 +0100

    Use Rails Cache for MeetupEvents rather than browser cache
    pairing with @apneadiving

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,5 +1,13 @@
 class MeetupsController < ApplicationController
   def index
-    
+    @meetup_events = meetup_events
+  end
+
+  private
+  
+  def meetup_events
+    Rails.cache.fetch(expires_in: 1.hour) do
+      MeetupEvent.all
+    end
   end
 end
