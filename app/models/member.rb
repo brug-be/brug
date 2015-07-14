c6f84c89dcc54453b1a8033a472125ae93706f66
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2015-07-14 20:49:44 +0200

    Remove last link/usage of participations/gatherings
    Issue #34

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,4 @@
 class Member < ActiveRecord::Base
-  has_many :participations, dependent: :destroy
-  has_many :gatherings, through: :participations
-  has_many :created_gatherings, class_name: 'Gathering', foreign_key: 'owner_id'
-
-  after_destroy :change_ownership_created_gatherings
-
   def self.from_omniauth(auth)
     where(auth.to_h.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
@@ -29,13 +23,9 @@ class Member < ActiveRecord::Base
     end
   end
 
-  def owns?(gathering)
-    created_gatherings.include? gathering
-  end
-
   def update_twitter_info
     avatar = TwitterClient.call.user(name).try(:profile_image_url).try(:to_s).try(:gsub, /_normal/, '')
-    update_column :avatar_url, avatar if avatar 
+    update_column :avatar_url, avatar if avatar
   rescue Twitter::Error::NotFound
     self.destroy
   end
@@ -44,8 +34,4 @@ class Member < ActiveRecord::Base
     "#{model_name.plural}/#{Digest::MD5.hexdigest(all.to_sql)}-#{count}-#{maximum(:updated_at).try(:utc).try(:to_s, :number)}"
   end
 
-  private
-  def change_ownership_created_gatherings
-    created_gatherings.update_all owner_id: Member.first.id
-  end
 end
